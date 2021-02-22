3d888e28a833ecc43bc6228f9090164d9f020295
梅文超 <171314187@qq.com>
2021-02-22 19:07:20 +0800

    初始化login

---
+++ src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+ <div class="login_container">
+登录App
+ </div>
+</template>
+
+<script>
+export default {
+ props: {
+
+ },
+ data() {
+ return {
+
+ };
+ },
+ computed: {
+
+ },
+ created() {
+
+ },
+ mounted() {
+
+ },
+ watch: {
+
+ },
+ methods: {
+
+ },
+ components: {
+
+ },
+};
+</script>
+
+<style lang='less' >
+.login_container{
+  font-size: 400px;
+}
+</style>
